5818711588d46888adcc74468da2847bc6167150
genain <luizgenain@gmail.com>
2022-09-20 20:03:40 -0300

    topbar and botbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Login from './Login.vue';
+import Topbar from './components/Topbar.vue';
+import Botbar from './components/Botbar.vue';
 </script>
 
 <template>
-  <!-- <Login /> -->
+  <Topbar/>
   <RouterView />
+  <Botbar/>
 </template>
 
 <style scoped>
---
+++ src/components/Topbar.vue
@@ -0,0 +1,158 @@
+<script>
+
+</script>
+
+<template>
+    <div class="top-bar">
+        <span>
+            <ion-icon name="call-outline"></ion-icon> 4002 - 8922
+        </span>
+        <ul>
+            <li><a href="#">
+                    <ion-icon name="logo-facebook"></ion-icon>
+                </a></li>
+            <li><a href="#">
+                    <ion-icon name="logo-twitter"></ion-icon>
+                </a></li>
+            <li><a href="#">
+                    <ion-icon name="logo-instagram"></ion-icon>
+                </a></li>
+        </ul>
+    </div>
+
+    <nav>
+        <div class="logo">
+            <a href="#"><img src="../assets/quatro.png" alt="logo"></a>
+        </div>
+        <toggle>
+            <a href="#">
+                <ion-icon name="menu-outline"></ion-icon>
+            </a>
+        </toggle>
+        <ul class="menu">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">Rules</a></li>
+            <li><a href="#">Adoption</a></li>
+            <li><a href="#">Login</a></li>
+            <li><a href="#">Contact</a></li>
+        </ul>
+    </nav>
+
+</template>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+
+body {
+    font-family: "Poppins", "sans-serif";
+}
+
+a,
+a:hover {
+    text-decoration: none;
+}
+
+.top-bar {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 10px 25px;
+    background: rgb(253, 204, 97);
+}
+
+.top-bar span {
+    color: black;
+}
+
+.top-bar ul {
+    list-style: none;
+    display: flex;
+}
+
+.top-bar li {
+    margin: 0px 5px;
+}
+
+.top-bar a {
+    color: black;
+}
+
+.top-bar a:hover {
+    color: #fff;
+}
+
+nav {
+    background: #222;
+    padding: 5px 20px;
+    display: flex;
+    align-items: center;
+}
+
+nav a {
+    color: #fff;
+}
+
+nav a:hover {
+    color: rgb(253, 204, 97);
+}
+
+.logo {
+    flex: 1;
+    padding: 30px;
+}
+
+.logo a {
+    display: flex;
+    align-items: center;
+    font-size: 20px;
+}
+
+.logo a:hover {
+    color: #fff;
+}
+
+.logo img {
+    width: 110px;
+    margin-right: 10px;
+}
+
+.menu {
+    display: flex;
+    align-items: center;
+    list-style: none;
+}
+
+.menu li {
+    padding: 15px 10px;
+    font-size: 16px;
+}
+
+.toggle {
+    font-size: 30px;
+    display: none;
+}
+
+@media screen and (max-width: 800px) {
+    nav {
+        display: block;
+        position: relative;
+        padding: 15px 20px;
+    }
+
+    .menu {
+        margin-top: 15px;
+        display: none;
+    }
+
+    .menu.active,
+    .toggle {
+        display: block;
+    }
+
+    .toggle {
+        position: absolute;
+        top: 15px;
+        right: 20px;
+    }
+}
+</style>
